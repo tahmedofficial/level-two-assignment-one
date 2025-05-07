7216de17f39cec748ebebdcb2bd5dc4b9d32720c
tahmedofficial <tanvirofcuo@gmail.com>
2025-05-07 21:30:58 +0600

    finish problem 1 and 2

--- assignment-1.js
+++ assignment-1.js
@@ -1,4 +1,42 @@
-// Problem 1
 function formatString(input, toUpper) {
-    return "";
+    if (toUpper === false) {
+        return input.toLowerCase();
+    }
+    else {
+        return input.toUpperCase();
+    }
 }
+var carItem = [
+    {
+        title: "car",
+        rating: 4
+    },
+    {
+        title: "bike",
+        rating: 3
+    },
+    {
+        title: "ricsha",
+        rating: 2
+    },
+    {
+        title: "biman",
+        rating: 6
+    },
+    {
+        title: "halicopter",
+        rating: 5
+    },
+];
+function filterByRating(items) {
+    return items.filter(function (item) { return item.rating >= 4; });
+}
+var books = [
+    { title: "Book A", rating: 4.5 },
+    { title: "Book B", rating: 3.2 },
+    { title: "Book C", rating: 5.0 }
+];
+// filterByRating(books);
+// Output: [ { title: "Book A", rating: 4.5 }, { title: "Book C", rating: 5.0 } ]
+var result = filterByRating(carItem);
+console.log(result);
--- assignment-1.ts
+++ assignment-1.ts
@@ -1,8 +1,28 @@
-// Problem 1
 function formatString(input: string, toUpper?: boolean): string {
-    
-    return ""
+    if (toUpper === false) {
+        return input.toLowerCase();
+    } else {
+        return input.toUpperCase();
+    }
 }
+
+type Items = {
+    title: string;
+    rating: number
+}
+
+function filterByRating(items: Items[]): Items[] {
+    return items.filter(item => item.rating >= 4)
+}
+
+// Problem 3
+
+
+
+
+// const result = filterByRating(carItem)
+// console.log(result);
+
 
 
 
