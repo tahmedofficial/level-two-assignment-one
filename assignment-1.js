b4d34d44b5b2f3ff1c9520b1fb508e27ecf6a328
tahmedofficial <tanvirofcuo@gmail.com>
2025-05-07 22:30:20 +0600

    finish problem 4

--- assignment-1.js
+++ assignment-1.js
@@ -1,3 +1,18 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 function formatString(input, toUpper) {
     if (toUpper === false) {
         return input.toLowerCase();
@@ -9,7 +24,6 @@ function formatString(input, toUpper) {
 function filterByRating(items) {
     return items.filter(function (item) { return item.rating >= 4; });
 }
-// Problem 3
 function concatenateArrays() {
     var arrays = [];
     for (var _i = 0; _i < arguments.length; _i++) {
@@ -17,7 +31,31 @@ function concatenateArrays() {
     }
     return arrays.reduce(function (accumulator, currentValue) { return accumulator.concat(currentValue); }, []);
 }
-// Output: ["a", "b", "c"]
-// Output: [1, 2, 3, 4, 5]
-var result = concatenateArrays([1, 2], [3, 4], [5]);
-console.log(result);
+// Problem 3
+var Vehicle = /** @class */ (function () {
+    function Vehicle(make, year) {
+        this.make = make;
+        this.year = year;
+    }
+    Vehicle.prototype.getInfo = function () {
+        return "make ".concat(this.make, ", Year: ").concat(this.year);
+    };
+    return Vehicle;
+}());
+var Car = /** @class */ (function (_super) {
+    __extends(Car, _super);
+    function Car(make, year, model) {
+        var _this = _super.call(this, make, year) || this;
+        _this.model = model;
+        return _this;
+    }
+    Car.prototype.getModel = function () {
+        return "Model: ".concat(this.model);
+    };
+    return Car;
+}(Vehicle));
+var car = new Car("Toyota", 2020, "Corolla");
+console.log("info", car.getInfo());
+console.log("model", car.getModel());
+// const result = concatenateArrays([1, 2], [3, 4], [5]);
+// console.log(result);
--- assignment-1.ts
+++ assignment-1.ts
@@ -19,8 +19,31 @@ function concatenateArrays<T>(...arrays: T[][]): T[] {
     return arrays.reduce((accumulator, currentValue) => accumulator.concat(currentValue), [])
 }
 
-// Problem 3
+class Vehicle {
+    private make: string;
+    private year: number;
 
+    constructor(make: string, year: number) {
+        this.make = make;
+        this.year = year;
+    }
+
+    getInfo() {
+        return `make ${this.make}, Year: ${this.year}`
+    }
+}
+
+class Car extends Vehicle {
+    private model: string;
+
+    constructor(make: string, year: number, model: string) {
+        super(make, year);
+        this.model = model;
+    }
+    getModel() {
+        return `Model: ${this.model}`
+    }
+}
 
 
 
